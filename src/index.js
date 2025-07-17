e167f77bcab352082bec4de86115859d567c14e5
goblin225 <issaclevi225@gmail.com>
2025-07-17 20:50:43 +0530

    cors

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ const app = express();
 // Connect to MongoDB
 connectDB();
 
+// CORS Configuration
 const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:5174',
@@ -26,18 +27,23 @@ const corsOptions = {
       callback(new Error('Not allowed by CORS'));
     }
   },
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 };
 
 // Middlewares
 app.use(cors(corsOptions));
+app.options('*', cors(corsOptions)); 
+
 app.use(express.json());
 app.use(cookieParser());
-app.use('/api', router)
 
+// Routes
+app.use('/api', router);
+
+// Start Server
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
