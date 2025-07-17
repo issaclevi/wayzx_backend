ef399b4d250ed0644f7468ca1adaae0eefbf166c
goblin225 <issaclevi225@gmail.com>
2025-07-17 21:21:13 +0530

    edit cors policy

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 require('dotenv').config();
 const express = require('express');
-const connectDB = require('./config/database');
-const router = require('./routes');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const router = require('./routes');
+const connectDB = require('./config/database');
 
 const app = express();
-
-// Connect to MongoDB
 connectDB();
 
 const allowedOrigins = [
@@ -21,7 +19,7 @@ const corsOptions = {
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
-      callback(new Error('Not allowed by CORS: ' + origin));
+      callback(new Error('CORS Not Allowed: ' + origin));
     }
   },
   credentials: true,
@@ -29,14 +27,17 @@ const corsOptions = {
   allowedHeaders: ['Content-Type', 'Authorization'],
 };
 
-// Middlewares
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
-app.use('/api', router)
+app.use('/api', router);
+
+app.use((err, req, res, next) => {
+  if (err instanceof Error && err.message.startsWith('CORS')) {
+    return res.status(403).json({ message: err.message });
+  }
+  next(err);
+});
 
 const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
